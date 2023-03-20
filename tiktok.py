209738da2802bd8417aafda91e2a91f4b64ae923
Lure <125397983+lurebruh@users.noreply.github.com>
2023-03-20 18:02:44 +0100

    Update tiktok.py

--- tiktok.py
+++ tiktok.py
@@ -1,2 +1,2 @@
-#to buy send me an dm on discord lure#0069
+#to buy send me an dm on discord lure#3000
 #preview https://cdn.discordapp.com/attachments/1085218204567220325/1086760469097877565/image.png
